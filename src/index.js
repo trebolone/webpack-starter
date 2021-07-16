e506e788688cfac8ba9f895a767377e38ac2846e
Mario <serfikreik@gmail.com>
2021-07-16 13:26:58 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import {saludar} from './js/componentes.js';
+import './js/styles.css';
+
+const nombre = 'Mario'
+saludar(nombre);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtarct = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+ 
+ 
+module.exports = {
+ 
+    mode: 'development',
+
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /styles.css$/, 
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtarct.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            inject: 'body'
+        }),
+
+        new MiniCssExtarct ({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        }),
+    ]
+ 
+ 
+}
